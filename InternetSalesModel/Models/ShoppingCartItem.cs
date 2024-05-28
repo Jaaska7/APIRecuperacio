26582fac590784b45c9de1a2191a16fc83742776
Jaska <123942698+Jaaska7@users.noreply.github.com>
2024-05-28 17:10:57 +0200

    Create ShoppingCartItem.cs

---
+++ InternetSalesModel/Models/ShoppingCartItem.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InternetSalesModel.Models
+{
+    public class ShoppingCartItem
+    {
+        [Required]
+        public int ShoppingCartId { get; set; }
+
+        public virtual ShoppingCart ShoppingCart { get; set; }
+
+        [Required]
+        public int ItemId { get; set; }
+
+        public virtual Item Item { get; set; }
+    }
+}
