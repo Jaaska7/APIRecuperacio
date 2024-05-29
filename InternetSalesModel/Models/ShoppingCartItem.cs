d4c35aa74ec03d2adaaeb3bb5a5c6d8beb09209e
Jaska <123942698+Jaaska7@users.noreply.github.com>
2024-05-29 17:58:29 +0200

    Update ShoppingCartItem.cs

--- InternetSalesModel/Models/ShoppingCartItem.cs
+++ InternetSalesModel/Models/ShoppingCartItem.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace InternetSalesModel.Models
 {
@@ -8,12 +9,15 @@ namespace InternetSalesModel.Models
         [Required]
         public int ShoppingCartId { get; set; }
 
-        public virtual ShoppingCart ShoppingCart { get; set; }
+        [JsonIgnore]
+        public virtual ShoppingCart? ShoppingCart { get; set; }
 
         [Required]
         public int ItemId { get; set; }
 
-        public virtual Item Item { get; set; }
-        public int Quantity { get; internal set; }
+        [JsonIgnore]
+        public virtual Item? Item { get; set; }
+
+        public int Quantity { get; set; }
     }
 }
